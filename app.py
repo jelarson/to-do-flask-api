f6b3f394186370ed91c4bc1f792b4210102414f8
jelarson <jesslarson93@gmail.com>
2020-04-06 11:10:35 -0600

    added get route

--- app.py
+++ app.py
@@ -1,13 +1,53 @@
-import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABSE_URI"] = "sqlite:///" + \
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
     os.path.join(basedir, "app.sqlite")
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean)
+
+    def __init__(self, title, done):
+        self.title = title
+        self.done = done
+
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'done')
+
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Todo Flask API</h1>"
+
+# Get
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    all_todos = Todo.query.all()
+    result = todos_schema.dump(all_todos)
+    return jsonify(result)
+
+# POST
+# PUT / PATCH
+# Delete
+
 
 if __name__ == "__main__":
     app.debug = True
