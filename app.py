c5e6095c3de8552ee642cf240e4522f8beec1882
jelarson <jesslarson93@gmail.com>
2020-04-08 09:24:40 -0600

    added Procfile and dependencies for web app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
+CORS(app)
+heroku = Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
